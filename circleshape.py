29d4734a98b92872332093e7a48dc63fda1e84cd
element-box <element_box@proton.me>
2025-04-05 14:09:17 -0400

    Adding Asteroids, AsteroidField, and collision detection

---
+++ circleshape.py
@@ -0,0 +1,25 @@
+import pygame
+import sys
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def collision(self, other):
+        distance = self.position.distance_to(other.position)
+        if distance < (self.radius + other.radius):
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 import pygame
+
 from constants import *
+from player import Player
+from asteroid import Asteroid 
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
+    # Groups
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Containers
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("Black")
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
+
+        for ast in asteroids:
+            if ast.collision(player):
+                print("Game Over!")
+                return
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # milliseconds to seconds
 
 
 
