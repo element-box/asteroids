8097064c68057c2d0850a109a7916b076a8689c7
element-box <element_box@proton.me>
2025-04-05 16:50:27 -0400

    Adding TODO for extras in main.py

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from shot import Shot
 from asteroid import Asteroid 
 from asteroidfield import AsteroidField
 
+# TODO:
+# Add a scoring system
+# Implement multiple lives and respawning
+# Add an explosion effect for the asteroids
+# Add acceleration to the player movement
+# Make the objects wrap around the screen instead of disappearing
+# Add a background image
+# Create different weapon types
+# Make the asteroids lumpy instead of perfectly round
+# Make the ship have a triangular hit box instead of a circular one
+# Add a shield power-up
+# Add a speed power-up
+# Add bombs that can be dropped
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
